a0a6da6276a76f7974ab65c0780cf28cb909030a
marwanosama8888 <marwan.osama8888@gmail.com>
2022-07-04 17:19:37 +0200

    change to @admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,6 @@ class User extends Authenticatable implements FilamentUser
     ];
     public function canAccessFilament(): bool
     {
-        return str_ends_with($this->email, '@xxx.com');
+        return str_ends_with($this->email, '@admin.com');
     }
 }
